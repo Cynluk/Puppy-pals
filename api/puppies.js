62068e5460ce78be1cb641b2035580e7291d7395
Cynthia <cluk@dynamicsignal.com>
2022-11-20 06:45:28 -0800

    puppiesRouter

--- api/puppies.js
+++ api/puppies.js
@@ -6,9 +6,71 @@ const prisma = require("../prisma/prisma");
 router.get(
   "/",
   asyncErrorHandler(async (req, res, next) => {
-    const puppies = await prisma.puppies.findMany();
+    const puppies = await prisma.puppies.findMany({
+      include: {
+        puppies_tricks: {
+          include: {
+            tricks: true,
+          },
+        },
+      },
+    });
     res.send(puppies);
   })
 );
 
+router.get(
+  "/:puppyId",
+  asyncErrorHandler(async (req, res, next) => {
+    const singlePup = await prisma.puppies.findUnique({
+      where: {
+        id: +req.params.puppyId,
+      },
+      include: {
+        puppies_tricks: {
+          include: {
+            tricks: true,
+          },
+        },
+      },
+    });
+    res.send(singlePup);
+  })
+);
+
+router.post(
+  "/",
+  asyncErrorHandler(async (req, res, next) => {
+    //const {} = req.body;
+    const createdPup = await prisma.puppies.create({
+      data: req.body,
+    });
+    res.send(createdPup);
+  })
+);
+
+router.patch(
+  "/:puppyId",
+  asyncErrorHandler(async (req, res, next) => {
+    const updatedPup = await prisma.puppies.update({
+      where: {
+        id: +req.params.puppyId,
+      },
+      data: req.body,
+    });
+    res.send(updatedPup);
+  })
+);
+
+router.delete(
+  "/:puppyId",
+  asyncErrorHandler(async (req, res, next) => {
+    const deletedPup = await prisma.puppies.delete({
+      where: {
+        id: +req.params.puppyId,
+      },
+    });
+    res.send(deletedPup);
+  })
+);
 module.exports = router;
